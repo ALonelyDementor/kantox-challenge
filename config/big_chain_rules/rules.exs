15568e5dccdff3017f87e8c3427ed196ea2b951f
ALonelyDementor <github.july201@passinbox.com>
2025-10-18 15:25:41 +0100

    Prepare a configurable cart product rules

---
+++ config/big_chain_rules/rules.exs
@@ -0,0 +1,5 @@
+import Config
+
+config :big_chain, BigChain.Products.Rules,
+  max_products_per_order: 10,
+  allow_backorders: false
--- config/config.exs
+++ config/config.exs
@@ -61,6 +61,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :big_chain, BigChain.Products.Product,
+  products_table_name: :products
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+import_config "big_chain_rules/rules.exs"
