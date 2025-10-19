0c72a70ea30974ae68ba32b9e956346cb8426f91
ALonelyDementor <github.july201@passinbox.com>
2025-10-20 00:43:12 +0100

    Add missing rules map

--- config/big_chain_rules/rules.exs
+++ config/big_chain_rules/rules.exs
@@ -5,10 +5,9 @@ config :big_chain, BigChain.Cart.ProductRules,
   # since we need to know all the products. Also, it doesn't fell scalable enough.
   # A best approach feels like adding these rules on the database side, using triggers and rules.
   # And update the rules with migrations.
-  rules: [
+  rules:
     %{
-      "GR1": %{rule: :buy_one_get_one_free},
-      "SR1": %{rule: :discount_relative, amount: 10, for_each: 3},
-      "CF1": %{rule: :discount_absolute, percentage: 66, for_each: 3}
+      GR1: %{rule: :buy_one_get_one_free},
+      SR1: %{rule: :discount_relative, amount: 10, for_each: 3},
+      CF1: %{rule: :discount_absolute, percentage: 66, for_each: 3}
     }
-  ]
