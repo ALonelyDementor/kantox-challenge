e439cdef164588212bbffa38a24564498cd5c99a
ALonelyDementor <github.july201@passinbox.com>
2025-10-20 21:39:37 +0100

    Indentation

--- config/config.exs
+++ config/config.exs
@@ -61,8 +61,7 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-config :big_chain, BigChain.Products.Product,
-  products_table_name: :products
+config :big_chain, BigChain.Products.Product, products_table_name: :products
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
